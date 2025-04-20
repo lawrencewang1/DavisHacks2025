255b5ff1c34c0bdcaccbb9d296c18e2dc502d4e7
LawrenceWang <lwang07@berkeley.edu>
2025-04-19 22:35:36 -0700

    finished chat.py, waiting for flask

--- src/chat.py
+++ src/chat.py
@@ -1,9 +1,18 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import pandas as pd
+
+df = pd.read_csv('data/final_testing_expense.csv')
+csv_text = df.to_csv(index=False)
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-2.0-flash-lite')
+chat = model.start_chat(history=[])
 
+def generate_response(query:str):
+    prompt = csv_text + "\n Analyze the data above to answer the following query:\n" + query
+    response = model.generate_content(prompt)
+    return response.text
