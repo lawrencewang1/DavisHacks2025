eede55227fecca6d13c0034f94f0e303fe5f79bf
LawrenceWang <lwang07@berkeley.edu>
2025-04-20 01:20:58 -0700

    gg flask

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template
+from src import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 CORS(app)
+
+from src import routes
