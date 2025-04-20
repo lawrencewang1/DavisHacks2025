a25af71bd487010430323ac24ec86963ec988dc9
Snorihan <janus.sucharitakul@gmail.com>
2025-04-20 02:52:30 -0700

    HEATMAP (kinda)

--- src/views.py
+++ src/views.py
@@ -1,16 +1,22 @@
 import pandas as pd
+from flask import jsonify, request
+from src import app
 
+'''NOTEs TO SELF IMPLEMENT WITH FLASK LATER'''
 
 def read_final():
     df = pd.read_csv('data/final_testing_expense.csv')
     return df
 
 
-def toggle(toggle_value):
+@app.route('/api/education-data', methods=['GET'])
+def toggle():
+    toggle_value = request.args.get('toggle_value')
+
     if toggle_value == 'funding':
-        return funding_map()
+        return jsonify(funding_map())
     else:
-        return testing_map()
+        return jsonify(testing_map())
 
 
 def funding_map():
@@ -18,7 +24,7 @@ def funding_map():
     return_dict = {}
     for i in range(len(df)):
         return_dict[df['County Name'][i]] = df['Expense per ADA'][i]
-    return return_dict
+    return return_dict  
 
 
 def testing_map():
@@ -26,7 +32,7 @@ def testing_map():
     return_dict = {}
     for i in range(len(df)):
         return_dict[df['County Name'][i]] = {}
-        return_dict[df['County Name'][i]]['Mean Score'] = df['Mean Score'][i]
+        return_dict[df['County Name'][i]]['Mean Score'] = df['Mean Scale Score'][i]
         return_dict[df['County Name'][i]]['Percentage Passed'] = df['Percentage Standard Met and Above'][i]
         return_dict[df['County Name'][i]]['Value'] = df['Test Calculated Value'][i]
-    return return_dict
+    return return_dict 
