60a833d1a790d51d56603ec79a8dabe30c74c7f4
LawrenceWang <lwang07@berkeley.edu>
2025-04-20 02:15:31 -0700

    finished chatbot and fixed routes

--- src/chat.py
+++ src/chat.py
@@ -2,8 +2,6 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from src import app
-from flask import request, Response
 
 df = pd.read_csv('data/final_testing_expense.csv')
 csv_text = df.to_csv(index=False)
@@ -13,9 +11,7 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-2.0-flash-lite')
 
-@app.route('/map', methods=['POST'])
-def generate_response():
-    query = request.get_data(as_text=True)
-    prompt = csv_text + "\n Analyze the data above to answer the following query:\n" + query
+def generate_response(query):
+    prompt = csv_text + "\n Analyze the data above to answer the following query in 2 sentences MAX:\n" + query
     response = model.generate_content(prompt)
-    return Response(response.text, mimetype='text/plain')
+    return response.text
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,7 @@
 from src import app
-from flask import render_template
+from flask import render_template, request, Response
+from .chat import generate_response
+from .views import toggle
 
 @app.route("/")
 def home():
@@ -8,3 +10,14 @@ def home():
 @app.route('/map')
 def map():
     return render_template('map.html')
+
+@app.route('/chat', methods=['POST'])
+def generate():
+    query = request.get_data(as_text=True)
+    response = generate_response(query)
+    return Response(response, mimetype='text/plain')
+
+@app.route('/map-data', methods=['POST'])
+def get_map_data():
+    toggle_value = request.json.get('toggle_value')
+    return toggle(toggle_value)
