8a078c96a8f615dc5bb78a48b60a042a66671f6c
LawrenceWang <lwang07@berkeley.edu>
2025-04-20 01:42:09 -0700

    fixed flask

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 CORS(app)
 
 from src import routes
